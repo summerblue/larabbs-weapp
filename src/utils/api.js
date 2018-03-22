b772f92d9e6de1711904c373d63bf5c2aec088a5
Yu Li <liyu001989@gmail.com>
2018-03-22 22:41:21 +0800

    page user

--- src/utils/api.js
+++ src/utils/api.js
@@ -3,15 +3,17 @@ import wepy from 'wepy'
 const host = 'http://larabbs.test/api'
 
 const request = async (options) => {
-  wx.showLoading({title: '加载中'})
-  if (options.withAuth === true) {
-    options.header['Authorization'] = 'Bearer '+ wepy.getStorageSync('access_token')
+  if (typeof options === 'string') {
+    options = {
+      url: options
+    }
   }
+  wepy.showLoading({title: '加载中'})
 
   options.url = host + '/' + options.url
   let response = await wepy.request(options)
 
-  wx.hideLoading()
+  wepy.hideLoading()
 
   if (response.statusCode === 500) {
     wepy.showModal({
@@ -20,6 +22,58 @@ const request = async (options) => {
     })
   }
   return response
+}
+
+const authRequest = async (options) => {
+  if (typeof options === 'string') {
+    options = {
+      url: options
+    }
+  }
+
+  wepy.showLoading({title: '加载中'})
+
+  // 从缓存中取出 Token
+  let accessToken = await wepy.getStorageSync('access_token')
+  let expiredAt = await wepy.getStorageSync('access_token_expired_at')
+
+  // 如果 token 过期了，则调用刷新方法
+  if (accessToken && new Date().getTime() > expiredAt) {
+    let refreshResponse = await refresh(accessToken)
+
+    if (refreshResponse.statusCode === 200) {
+      accessToken = refreshResponse.data.access_token
+    } else {
+      // 刷新失败了，重新调用登录方法，设置 Token
+      let authResponse = await login()
+      if (authResponse.statusCode === 201) {
+        accessToken = authResponse.data.access_token
+      }
+    }
+  }
+
+  let header = options.header || {}
+  header.Authorization = 'Bearer ' + accessToken
+  options.header = header
+
+  return request(options)
+}
+
+const refresh = async (accessToken) => {
+  let refreshResponse = await wepy.request({
+    url: host + '/' + 'authorizations/current',
+    method: 'PUT',
+    header: {
+      'Authorization': 'Bearer ' + accessToken
+    }
+  })
+
+  if (refreshResponse.statusCode === 200) {
+    wepy.setStorageSync('access_token', refreshResponse.data.access_token)
+    wepy.setStorageSync('access_token_expired_at', new Date().getTime() + refreshResponse.data.expires_in * 1000)
+  }
+
+  return refreshResponse
 }
 
 const login = async (params = {}) => {
@@ -44,7 +98,27 @@ const login = async (params = {}) => {
   return authResponse
 }
 
+const logout = async (params = {}) => {
+  let accessToken = await wepy.getStorageSync('access_token')
+
+  let logoutResponse = wepy.request({
+    url: host + '/' + 'authorizations/current',
+    method: 'DELETE',
+    header: {
+      'Authorization': 'Bearer ' + accessToken
+    }
+  })
+
+  if (logoutResponse) {
+    wepy.clearStorage()
+  }
+
+  return logoutResponse
+}
+
 module.exports = {
   request,
-  login
+  authRequest,
+  login,
+  logout
 }
