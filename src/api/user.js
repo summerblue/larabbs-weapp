3d8ca31e04c1ab659ad40e5306f78a4c7805fec7
liyu <liyu001989@gmail.com>
2020-03-31 23:34:37 +0800

    update avatar

--- src/api/user.js
+++ src/api/user.js
@@ -1,4 +1,4 @@
-import { request, authRequest } from '@/utils/request'
+import { request, authRequest, uploadFile } from '@/utils/request'
 
 export function getCurrentUser(data) {
   return authRequest('user')
@@ -10,3 +10,15 @@ export function updateUser(data) {
     data: data
   })
 }
+
+export function updateAvatar(avatar) {
+  return uploadFile('images', {
+    method: 'POST',
+    name: 'image',
+    formData: {
+      type: 'avatar'
+    },
+    filePath: avatar
+  })
+}
+
