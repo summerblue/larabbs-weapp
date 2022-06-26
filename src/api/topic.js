120c1777dad0a84267cc60249eb827fc3efacb47
Yu Li <liyu001989@gmail.com>
2022-06-26 23:08:40 +0800

    topic delete

--- src/api/topic.js
+++ src/api/topic.js
@@ -1,4 +1,4 @@
-import { request } from '@/utils/request'
+import { request, authRequest } from '@/utils/request'
 
 export function getTopics(data) {
   return request('topics', {
@@ -18,6 +18,13 @@ export function getTopic(id, data) {
 
 export function getUserTopics(userId, data) {
   return request('users/'+userId+'/topics', {
+    data: data
+  })
+}
+
+export function deleteTopic(id, data) {
+  return authRequest('topics/' + id, {
+    method: 'DELETE',
     data: data
   })
 }
